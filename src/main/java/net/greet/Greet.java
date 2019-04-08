707f692a7fc66e30ddecd6a60db195bb6fa0eb49
RichardMarkRoss <cptrichkidprice@gmail.com>
2019-04-08 16:48:29 +0200

    need to create test for greet

--- src/main/java/net/greet/Greet.java
+++ src/main/java/net/greet/Greet.java
@@ -9,48 +9,62 @@ public class Greet {
     Map<String, Integer> hash = new HashMap<String, Integer>();
     private String username;
     private String language;
-    private String greet;
+    private String commands;
 
-    public void stringToArgs(String name){
+    public void greet(String name){
         String[] elements = name.split("\\s");
-
-        this.greet = elements[0];
+        this.commands = elements[0];
         this.username = elements[1];
         this.language = elements[2];
 
-        System.out.println(this.greet);
-        System.out.println(this.username);
-        System.out.println(this.language);
-    }
-
-    public void greet(){
-
-        if (!hash.containsKey(username)) {
+        if (!hash.containsKey(username)){
             this.hash.put(username, 0);
+            System.out.println("Hello " + username);
         }
-
         if (language != "") {
-            // counter++;
             int counterForUser = this.hash.get(username);
             counterForUser++;
             this.hash.put(username, counterForUser);
+
+            if(language.equals("english")){
+                System.out.println("Hello " + username);
+            }else if(language.equals("afrikaans")){
+                System.out.println("goeie dag " + username);
+            }else if(language.equals("xhosa")){
+                System.out.println("Molo" + username);
+            }
 
             System.out.println("your name has been successfully greeted!");
         }
     }
 
-    public void Greeted(String name) {
+    public void greeted(String username) {
         this.username = username;
         if (hash.containsKey(username)) {
-            System.out.println(username);
+           hash.get(username).equals(username);
+            int mapSize = hash.get(username.length());
+            System.out.println(username + " has been greeted: " + mapSize);
+        }
+        if (!hash.containsKey(username)){
+            for (Map.Entry<String, Integer> entry : hash.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                System.out.println(value + " has been greeted " + key + " times");
+            }
         }
     }
 
-    public void Counter() {
-
+    public void counter(String username) {
+        this.username = username;
+        if(hash.containsKey(username)){
+            hash.containsKey(username.length());
+        }
+        if(!hash.containsKey(username));
+        hash.size();
     }
 
-    public void Clear() {
+    public void clear(String username) {
+        this.username = username;
         if(username != ""){
             hash.remove(hash.containsKey(username));
         }
@@ -59,7 +73,8 @@ public class Greet {
         }
     }
 
-    public String Help() {
+    public String help() {
+
         return "greet- followed by the name and the language the user is to be greeted in,\n" +
                 "greeted- should display a list of all users that has been greeted and how many time each user has been greeted,\n" +
                 "greeted- followed by a username returns how many times that username have been greeted,\n" +
@@ -68,25 +83,41 @@ public class Greet {
                 "clear- followed by a username delete the greet counter for the specified user and decrement the greet counter by 1,\n" +
                 "exit- exits the application,\n" +
                 "help- shows a user an overview of all possible commands.";
-    }
 
-    public void Exit() {
     }
-
     public static void main(String[] args) {
+
         Scanner insert = new Scanner(System.in);
-        Greet meth = new Greet();
+        Greet method = new Greet();
+        System.out.println("type help for more commands");
 
-        // while not typed exit
-        while(!insert.equals("exit")){
+        while(!("exit".equals(insert))){
+
+            String userInput = insert.nextLine();
+            String[] elements = userInput.split("\\s");
             System.out.println("Please insert a username and/or language.");
-            String name = insert.nextLine();
 
-            meth.stringToArgs(name);
-            meth.greet();
-            System.out.println(meth.greet.toString());
-//            System.out.println(meth.hash);
+            if  (elements[0].equals("greet")){
+
+                method.greet(userInput);
+            }else if(elements[0].equals("help")){
+
+                method.help();
+            }else if(elements[0].equals("greeted")){
+                System.out.println("Greeted user :");
+            method.greeted(elements[1]);
+            }
+            else if(elements[0].equals("clear")){
+                System.out.println("Clear user :");
+                method.clear(elements[1]);
+            }else if(elements[0].equals("count")){
+                System.out.println("Count user :");
+                method.counter(elements[1]);
+            }else{
+                System.out.println("please type help to view valid commands");
+            }
         }
-    }
 
+        System.out.println("Thank you for your time!");
+    }
 }
