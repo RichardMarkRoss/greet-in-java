8a3725d904025c3ccc6f5e93b8f79e9268358176
RichardMarkRoss <cptrichkidprice@gmail.com>
2019-04-12 16:37:37 +0200

    adds needed

--- src/main/java/net/greet/Greet.java
+++ src/main/java/net/greet/Greet.java
@@ -7,30 +7,29 @@ import java.util.Scanner;
 public class Greet {
     //private int counter = 0;
     Map<String, Integer> hash = new HashMap<String, Integer>();
-    private String username;
-    private String language;
-    private String commands;
+    public String username = "";
+    public String language = "";
+//    public String commands;
 
-    public void greet(String name){
+    public void greet(String name) {
         String[] elements = name.split("\\s");
-        this.commands = elements[0];
         this.username = elements[1];
         this.language = elements[2];
 
-        if (!hash.containsKey(username)){
+        if (!hash.containsKey(username)) {
             this.hash.put(username, 0);
-            System.out.println("Hello " + username);
+//            System.out.println("Hello " + username);
         }
-        if (language != "") {
+        if (language != "" && hash.containsKey(username)) {
             int counterForUser = this.hash.get(username);
             counterForUser++;
             this.hash.put(username, counterForUser);
 
-            if(language.equals("english")){
+            if (language.equals("english")) {
                 System.out.println("Hello " + username);
-            }else if(language.equals("afrikaans")){
+            } else if (language.equals("afrikaans")) {
                 System.out.println("goeie dag " + username);
-            }else if(language.equals("xhosa")){
+            } else if (language.equals("xhosa")) {
                 System.out.println("Molo" + username);
             }
 
@@ -39,43 +38,41 @@ public class Greet {
     }
 
     public void greeted(String username) {
-        this.username = username;
-        if (hash.containsKey(username)) {
-           hash.get(username).equals(username);
-            int mapSize = hash.get(username.length());
-            System.out.println(username + " has been greeted: " + mapSize);
-        }
-        if (!hash.containsKey(username)){
-            for (Map.Entry<String, Integer> entry : hash.entrySet()) {
-                String key = entry.getKey();
-                Integer value = entry.getValue();
-                System.out.println(value + " has been greeted " + key + " times");
-            }
-        }
+        String Username = username;
+        hash.get(Username).equals(Username);
+        int mapSize = hash.get(Username);
+        System.out.println(Username + " has been greeted: " + mapSize);
+
+
     }
 
-    public void counter(String username) {
-        this.username = username;
-        if(hash.containsKey(username)){
-            hash.containsKey(username.length());
+    public void greetedAll() {
+
+        for (HashMap.Entry<String, Integer> entry : hash.entrySet()) {
+            String key = entry.getKey();
+            System.out.println(key);
+            Integer value = entry.getValue();
+            System.out.println(value);
+            System.out.println(value + " has been greeted " + key + " times");
         }
-        if(!hash.containsKey(username));
-        hash.size();
+
+    }
+
+    public void counter() {
+
+        System.out.println("amount of user are : " + hash.size());
     }
 
     public void clear(String username) {
-        this.username = username;
-        if(username != ""){
-            hash.remove(hash.containsKey(username));
-        }
-        else{
+        if (username != "") {
+            hash.remove(username);
+        } else {
             hash.clear();
         }
     }
 
     public String help() {
-
-        return "greet- followed by the name and the language the user is to be greeted in,\n" +
+        String help = "greet- followed by the name and the language the user is to be greeted in,\n" +
                 "greeted- should display a list of all users that has been greeted and how many time each user has been greeted,\n" +
                 "greeted- followed by a username returns how many times that username have been greeted,\n" +
                 "counter- returns a count of how many unique users has been greeted,\n" +
@@ -83,37 +80,44 @@ public class Greet {
                 "clear- followed by a username delete the greet counter for the specified user and decrement the greet counter by 1,\n" +
                 "exit- exits the application,\n" +
                 "help- shows a user an overview of all possible commands.";
-
+        System.out.println(help);
+        return help;
     }
+
     public static void main(String[] args) {
 
         Scanner insert = new Scanner(System.in);
         Greet method = new Greet();
         System.out.println("type help for more commands");
 
-        while(!("exit".equals(insert))){
+        while (!insert.equals("exit")) {
 
             String userInput = insert.nextLine();
             String[] elements = userInput.split("\\s");
-            System.out.println("Please insert a username and/or language.");
 
-            if  (elements[0].equals("greet")){
+            if (elements[0].equals("greet")) {
 
                 method.greet(userInput);
-            }else if(elements[0].equals("help")){
 
+            } else if (elements[0].equals("help")) {
                 method.help();
-            }else if(elements[0].equals("greeted")){
-                System.out.println("Greeted user :");
-            method.greeted(elements[1]);
-            }
-            else if(elements[0].equals("clear")){
+
+            } else if (elements[0].equals("greeted")) {
+                String userIn = elements[1];
+                if (userIn.equals(null)){
+                    method.greetedAll();
+                } else {
+                    method.greeted(userIn);
+                }
+            } else if (elements[0].equals("clear")) {
                 System.out.println("Clear user :");
                 method.clear(elements[1]);
-            }else if(elements[0].equals("count")){
+
+            } else if (elements[0].equals("count")) {
                 System.out.println("Count user :");
-                method.counter(elements[1]);
-            }else{
+                method.counter();
+
+            } else {
                 System.out.println("please type help to view valid commands");
             }
         }
